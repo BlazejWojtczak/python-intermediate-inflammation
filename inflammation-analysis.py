e97edca763bd94f6d1449072ddb16d7537e96d22
Blazej <blazej.wojtczak@gmail.com>
2023-05-26 08:55:52 +0200

    Code style fixes.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -7,28 +7,25 @@ from inflammation import models, views
 
 
 def main(args):
-    """The MVC Controller of the patient inflammation data system.
 
-    The Controller is responsible for:
-    - selecting the necessary models and views for the current task
-    - passing data between models and views
-    """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+    infiles = args.infiles
+    if not isinstance(infiles, list):
+        infiles = [args.infiles]
 
-
-    for filename in InFiles:
+    for filename in infiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)
+        }
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='A basic patient inflammation data management system')
-
     parser.add_argument(
         'infiles',
         nargs='+',
